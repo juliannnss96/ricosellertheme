c4d1c46544bbdb64ee87ce2a96a3565ddfc5219c
julian_gomara <juliannnwb@gmail.com>
2025-01-21 16:30:06 -0300

    home full

--- assets/custom.js
+++ assets/custom.js
@@ -43,7 +43,6 @@ $(document).ready(function() {
 
 	/*Modal guia de talles single product*/
 	$(document).on('click', '.open-guia-talles-variantes', function() {
-		console.log('a ve');
 		$('.modal-guia-de-talles').addClass('abierto');
 	});
 
@@ -55,5 +54,38 @@ $(document).ready(function() {
 	  if (e.target !== this)
 	    return;
 	 	$('.modal-guia-de-talles').removeClass('abierto');
+	});
+
+	/*Modal redes home*/
+	$(document).on('click', '.open-iframe-redes', function(e) {
+		e.preventDefault();
+		var iframe = $(this).attr('data-iframe');
+		$('.modal-iframe-redes .modal .content').html('<iframe src="https://www.instagram.com/p/'+iframe+'/embed/" scrolling="no" allowtransparency="true" allowfullscreen="true" frameborder="0">Iframe incompatible</iframe>');
+		$('.modal-iframe-redes').addClass('abierto');
+	});
+
+	$('.modal-iframe-redes .modal .cerrar').click(function() {
+		$('.modal-iframe-redes').removeClass('abierto');
+	});
+
+	$('.modal-iframe-redes').on('click', function(e) {
+	  if (e.target !== this)
+	    return;
+	 	$('.modal-iframe-redes').removeClass('abierto');
+	});
+
+	/*Modal default*/
+	$(document).on('click', '.open-modal-default', function() {
+		$('.modal-default').addClass('abierto');
+	});
+
+	$('.modal-default .modal .cerrar').click(function() {
+		$('.modal-default').removeClass('abierto');
+	});
+
+	$('.modal-default').on('click', function(e) {
+	  if (e.target !== this)
+	    return;
+	 	$('.modal-default').removeClass('abierto');
 	});
 });
