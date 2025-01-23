31525ccd6b502c77c7b2a21383498d4a03ec82d2
julian_gomara <juliannnwb@gmail.com>
2025-01-22 23:31:42 -0300

    responsive & password page

--- assets/custom.js
+++ assets/custom.js
@@ -56,6 +56,21 @@ $(document).ready(function() {
 	 	$('.modal-guia-de-talles').removeClass('abierto');
 	});
 
+	/*Menu mobile*/
+	$('.hamburger-mobile').click(function() {
+		$('.menu-mobile').addClass('active');
+	});
+
+	$('.menu-mobile .contenedor .cerrar').click(function() {
+		$('.menu-mobile').removeClass('active');
+	});
+
+	$('.menu-mobile').on('click', function(e) {
+	  if (e.target !== this)
+	    return;
+	 	$('.menu-mobile').removeClass('active');
+	});
+
 	/*Modal redes home*/
 	$(document).on('click', '.open-iframe-redes', function(e) {
 		e.preventDefault();
@@ -77,12 +92,14 @@ $(document).ready(function() {
 	});
 
 	/*Modal default*/
-	$(document).on('click', '.open-modal-default', function() {
-		$('.modal-default').addClass('abierto');
+	$(document).on('click', '.open-modal', function() {
+		var modal_class = $(this).attr('data-modal');
+		$('.'+modal_class).addClass('abierto');
 	});
 
 	$('.modal-default .modal .cerrar').click(function() {
-		$('.modal-default').removeClass('abierto');
+		var modal_class = $(this).attr('data-modal');
+		$('.'+modal_class).removeClass('abierto');
 	});
 
 	$('.modal-default').on('click', function(e) {
